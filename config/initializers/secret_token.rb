ba38ef65f4fccc5f6691a02a42429567769ba929
Zachary Nicoll <tower@zdn.me>
2012-08-25 03:53:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unlockherfreedom::Application.config.secret_token = 'fa715163d655cf937b6b99d9fa6bc55a52d993006676d1cd7e1fd2b15daf5e39ff11339381691c11b000b1b47f839e6966827d8703b92dbaaf5de01029e33114'
