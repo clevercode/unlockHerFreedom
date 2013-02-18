e1a36cfd8b8df77cc77562b6b93d270ee6992805
Zachary Nicoll <tower@zdn.me>
2013-02-17 22:39:55 -0500

    Changed amounts to inetgers

---
+++ db/migrate/20130218025409_change_amount_type_for_payments.rb
@@ -0,0 +1,9 @@
+class ChangeAmountTypeForPayments < ActiveRecord::Migration
+  def up
+    change_column :payments, :amount, :integer
+  end
+
+  def down
+    change_column :payments, :amount, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121228055805) do
+ActiveRecord::Schema.define(:version => 20130218025409) do
 
   create_table "conversations", :force => true do |t|
     t.string   "name"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(:version => 20121228055805) do
   end
 
   create_table "payments", :force => true do |t|
-    t.float    "amount"
+    t.integer  "amount"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
