4ca3e83faae49437d4682f7e56097bff1c6d8d57
Zachary Nicoll <tower@zdn.me>
2013-02-18 02:06:44 -0500

    Removed ze period

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,7 +5,7 @@ class AdminMailer < ActionMailer::Base
     @message = message
     mail(
       from: "#{@message.messenger_name} <#{@message.messenger_email}>",
-      subject: 'Message for the UHF team.'
+      subject: 'Message for the UHF team'
     )
   end
 end
