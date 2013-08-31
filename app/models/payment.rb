9044627a9412b5f706116a4fb3cbe9a2f6634973
Zachary Nicoll <tower@zdn.me>
2013-08-31 11:17:34 -0400

    Added a formatted amount method to payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,9 @@
 class Payment < ActiveRecord::Base
   attr_accessible :amount, :email
   validates_presence_of :email, :amount
+
+  def formatted_amount
+    Money.new(self.amount, 'USD').to_s
+  end
+
 end
